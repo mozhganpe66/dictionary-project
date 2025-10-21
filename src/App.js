0c6ec02cbde1ed3ba7279bb481570b031d40b94a
mozhganpe66 <mozhgan.pe66@gmail.com>
2025-10-21 18:30:25 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import shecodesLogo from './shecodesLogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={shecodesLogo} className="App-logo img-fluid" alt="logo" /> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
