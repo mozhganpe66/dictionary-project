a24b11e5258a54d56ae17dcf6191398bdcaf493c
mozhganpe66 <mozhgan.pe66@gmail.com>
2025-10-22 10:50:13 +0200

    created Dictionary search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import shecodesLogo from './shecodesLogo.png';
+import Dictionary from './Dictionary'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={shecodesLogo} className="App-logo img-fluid" alt="logo" /> 
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>coded by Shecodes</small></footer>
+      </div>
     </div>
   );
 }
